aeccea507217569511c98853356ee661f34bb7a8
Eric Feuerstein <esteinsci@gmail.com>
2021-11-29 22:18:12 -0800

    login-page: Implement (rudimentary) login logic

--- frontend/components/Input/Input.js
+++ frontend/components/Input/Input.js
@@ -1,14 +1,25 @@
 import styles from '../../styles/Login.module.sass';
 
-const Input = ({ type, name, required }) => {
+const Input = ({ type, name, required, setValue, isFormValid, label }) => {
   return (
-    <input
-      className={styles.creds}
-      type={type}
-      name={name}
-      required={required}
-      maxLength={25}
-    />
+    <>
+      {label && (
+        <>
+          <label htmlFor={name}>
+            <b>{label}</b>
+          </label>
+          <br />
+        </>
+      )}
+      <input
+        className={styles.creds + ' ' + (!isFormValid && styles.invalidInput)}
+        type={type}
+        name={name}
+        required={required}
+        onChange={(e) => setValue(e.target.value)}
+        maxLength={25}
+      />
+    </>
   );
 };
 
--- frontend/components/Login/LoginForm.js
+++ frontend/components/Login/LoginForm.js
@@ -1,67 +1,97 @@
 import styles from '../../styles/Login.module.sass';
 import { useState, useEffect } from 'react';
 import Input from '../Input/Input';
+import { useRouter } from 'next/router';
+import { MdLightbulbOutline } from 'react-icons/md';
 
 const LoginForm = () => {
-  const [isUsernameFilled, setIsUsernameFilled] = useState(false);
-  const [isPasswordFilled, setIsPasswordFilled] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isFormValid, setisFormValid] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     setIsLoaded(true);
   }, []);
 
+  const redirectToUserPage = (userData) => {
+    router.push('/user');
+  };
+
   const submitCreds = () => {
-    if (!isUsernameFilled || !isPasswordFilled) {
+    if (!username || !password) {
+      setisFormValid(false);
       return;
     }
 
-    fetch('/api/login')
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+    setisFormValid(true);
+    setIsSubmitting(true);
+
+    const request = { username: username, password: password };
+    fetch('/api/login', { method: 'POST', body: request })
+      .then(async (response) => {
+        if (response.status === 200) {
+          const data = await response.json();
+          redirectToUserPage(data);
+        } else if (response.status === 401) {
+          highlightInputs();
+        }
+      })
+      .then((data) => onSuccess(data))
+      .catch((err) => {
+        console.error(err);
+        setisFormValid(false);
+        setIsSubmitting(false);
+      });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitCreds();
+  };
   return (
-    <form className={styles.form} method='post'>
+    <form className={styles.form}>
+      <span className={styles.loginText}>Log in</span>
       <div className={styles.username}>
-        <label for='uname'>
-          <b>Username</b>
-        </label>
-        <br />
         <Input
           type='text'
           name='uname'
           required={true}
-          onChange={setIsUsernameFilled}
+          setValue={setUsername}
+          isFormValid={isFormValid}
+          label='Username'
         />
       </div>
       <div className={styles.password}>
-        <label for='psw'>
-          <b>Password</b>
-        </label>
-        <br />
         <Input
           type='password'
           name='psw'
           required={true}
-          onChange={setIsPasswordFilled}
+          setValue={setPassword}
+          isFormValid={isFormValid}
+          label='Password'
         />
       </div>
-      <br />
-      <p className={styles.loginText}>Login?</p>
       <button
         className={styles.loginButton}
-        onClick={submitCreds}
+        onClick={handleSubmit}
         type='submit'
       >
         -&gt;
       </button>
       <span
         className={
-          styles.lightController + ' ' + (isLoaded && styles.animateLight)
+          styles.lightController +
+          ' ' +
+          (isSubmitting ? styles.submitLight : isLoaded && styles.loadedLight)
         }
       ></span>
+
+      <span className={styles.spinner}>
+        <MdLightbulbOutline />
+      </span>
     </form>
   );
 };
