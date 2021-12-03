c4475e2ed556c10c675ac0b1fd0a331908e6f7e7
Eric Feuerstein <esteinsci@gmail.com>
2021-12-02 22:34:21 -0800

    dashboard-page: Add test API call and adjust hover styling

--- frontend/components/IdeaList/IdeaList.js
+++ frontend/components/IdeaList/IdeaList.js
@@ -1,11 +1,24 @@
 import IdeaCard from '../IdeaCard/IdeaCard';
 import { useEffect, useState } from 'react';
+import router from 'next/router';
+import styles from '../../styles/List.module.sass';
 
 const IdeaList = () => {
   const [ideaList, setIdeaList] = useState([]);
 
   const getIdeas = () => {
-    setIdeaList(['Laptarp®', 'Ideaverse']);
+    fetch('/api/ideas', { method: 'GET' })
+      .then(async (response) => {
+        if (response.status === 200) {
+          const data = await response.json();
+          setIdeaList(data);
+        } else if (response.status === 401) {
+          router.push('/');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   useEffect(() => {
@@ -13,11 +26,11 @@ const IdeaList = () => {
   }, []);
 
   return (
-    <ul>
+    <ul className={styles.list} tabIndex={0}>
       {ideaList.map((idea, i) => {
         return (
           <li>
-            <IdeaCard name={idea} position={i} key={i} />
+            <IdeaCard name={idea.name} position={i} key={i} />
           </li>
         );
       })}
