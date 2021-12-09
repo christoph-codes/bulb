94fe8475eaf38c3df0008c2f32ec83c49618baca
Eric Feuerstein <esteinsci@gmail.com>
2021-12-08 19:04:35 -0800

    dashboard-page: Implement all colors in settings

--- frontend/components/IdeaNote/IdeaNote.js
+++ frontend/components/IdeaNote/IdeaNote.js
@@ -1,21 +1,59 @@
-import { useEffect, useState } from 'react';
 import styles from '../../styles/Note.module.sass';
+import { MdSettings } from 'react-icons/md';
+import { useState } from 'react';
 
-const IdeaNote = ({ name, description, position }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
+const IdeaNote = ({ name, description }) => {
+  const [isSettingsActive, setIsSettingsActive] = useState(false);
+  const [color, setColor] = useState('yellow');
+  const availableColors = [
+    'yellow',
+    'blue',
+    'green',
+    'purple',
+    'pink',
+    'orange',
+  ];
 
-  useEffect(() => {
-    const delay = 100 + 100 * position;
-
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, delay);
-  }, []);
+  const changeColor = (selectedColor) => {
+    setColor(selectedColor);
+  };
 
   return (
-    <div className={styles.note + (!isLoaded ? ` ${styles.loading}` : '')}>
-      <h2>{name}</h2>
-      <p className={styles.description}>{description}</p>
+    <div
+      className={styles.note + ' ' + styles[color]}
+      onMouseLeave={() => setIsSettingsActive(false)}
+    >
+      {isSettingsActive && (
+        <div className={styles.settings}>
+          Colors:
+          <ul>
+            {availableColors.map((color) => {
+              return (
+                <li>
+                  <div
+                    className={styles[color]}
+                    onClick={() => changeColor(color)}
+                  ></div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+      <div className={styles.cogContainer}>
+        <span
+          className={
+            styles.cog + (isSettingsActive ? ' ' + styles.cogActive : '')
+          }
+          onMouseOver={() => setIsSettingsActive(true)}
+        >
+          <MdSettings />
+        </span>
+      </div>
+      <div onMouseEnter={() => setIsSettingsActive(false)}>
+        <h2>{name}</h2>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
