b013162ade59dd0edd1fafc3aa7e8c0ebf8ee453
Christopher Jones <tkcwebdev@gmail.com>
2022-03-28 22:15:28 -0700

    13: Update port to be 5555 since 5000 is reserved fby Apple

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const BULB_API = axios.create({
 	baseURL:
 		process.env.NODE_ENV === 'development'
-			? 'http://localhost:5000'
+			? 'http://localhost:5555'
 			: process.env.NEXT_PUBLIC_BULB_API_URL,
 });
 
