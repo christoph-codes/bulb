8a4177f548ae46939aea1c4fedbdf5e548edd76a
Christopher Jones <tkcwebdev@gmail.com>
2022-07-15 22:29:00 -0700

    13: Create login functions on api page for testing and setup auth provider

--- frontend/pages/api-functions/index.js
+++ frontend/pages/api-functions/index.js
@@ -1,211 +1,189 @@
-import { useRef, useState } from 'react';
-import { useAuth } from '../../providers/auth';
-import styles from './api-functions.module.scss';
+import { useState } from "react";
+import { useAuth } from "../../providers/auth";
+import styles from "./api-functions.module.scss";
 
 const Apis = ({ props }) => {
-	const { login, createAccount, user, error } = useAuth();
-	const [result, setResult] = useState({});
-	const [loginFormInputs, setLoginFormInputs] = useState({
-		email: '',
-		password: '',
-	});
-	const [createAccountFormInputs, setCreateAccountFormInputs] = useState({
-		fname: '',
-		lname: '',
-		email: '',
-		password: '',
-	});
-	const [updateAccountFormInputs, setUpdateAccountFormInputs] = useState({
-		fname: user.fname,
-		lname: user.lname,
-		email: user.email,
-	});
-	const update = (e, inputs, setFunction) => {
-		const {
-			target: { name, value },
-		} = e;
-		setFunction({ ...inputs, [name]: value });
-	};
-	const validate = (e) => {
-		console.log(e.target.value === 'hello');
-	};
-	return (
-		<div className={styles.Api}>
-			<h1>API Functions</h1>
-			{error && <p>{JSON.stringify(error)}</p>}
-			<div className={styles.Api__cards}>
-				<article className={styles.Api__cards__card}>
-					<h2>Login</h2>
-					{/* Testing the login function. TODO: Delete this */}
-					<input
-						type='text'
-						name='email'
-						placeholder='Email'
-						value={loginFormInputs.email}
-						onChange={(e) =>
-							update(e, loginFormInputs, setLoginFormInputs)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='password'
-						name='password'
-						placeholder='Password'
-						value={loginFormInputs.password}
-						onChange={(e) =>
-							update(e, loginFormInputs, setLoginFormInputs)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<button
-						onClick={() =>
-							login(
-								loginFormInputs.email,
-								loginFormInputs.password,
-							)
-						}
-					>
-						Login
-					</button>
-					<h4>Result:</h4>
-					{result && JSON.stringify(result)}
-				</article>
-			</div>
-			<div className={styles.Api__cards}>
-				<article className={styles.Api__cards__card}>
-					<h2>Create Account</h2>
-					{/* Testing the login function. TODO: Delete this */}
-					<input
-						type='text'
-						name='fname'
-						placeholder='First Name'
-						value={createAccountFormInputs.fname}
-						onChange={(e) =>
-							update(
-								e,
-								createAccountFormInputs,
-								setCreateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='text'
-						name='lname'
-						placeholder='Last Name'
-						value={createAccountFormInputs.lname}
-						onChange={(e) =>
-							update(
-								e,
-								createAccountFormInputs,
-								setCreateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='text'
-						name='email'
-						placeholder='Email'
-						value={createAccountFormInputs.email}
-						onChange={(e) =>
-							update(
-								e,
-								createAccountFormInputs,
-								setCreateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='password'
-						name='password'
-						placeholder='Password'
-						value={createAccountFormInputs.password}
-						onChange={(e) =>
-							update(
-								e,
-								createAccountFormInputs,
-								setCreateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<button
-						onClick={() =>
-							createAccount(
-								createAccountFormInputs.fname,
-								createAccountFormInputs.lname,
-								createAccountFormInputs.email,
-								createAccountFormInputs.password,
-							)
-						}
-					>
-						Create Account
-					</button>
-				</article>
-			</div>
-			<div className={styles.Api__cards}>
-				<article className={styles.Api__cards__card}>
-					<h2>Update Account</h2>
-					{/* Testing the login function. TODO: Delete this */}
-					<input
-						type='text'
-						name='fname'
-						placeholder='First Name'
-						value={updateAccountFormInputs.fname}
-						onChange={(e) =>
-							update(
-								e,
-								updateAccountFormInputs,
-								setUpdateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='text'
-						name='lname'
-						placeholder='Last Name'
-						value={updateAccountFormInputs.lname}
-						onChange={(e) =>
-							update(
-								e,
-								updateAccountFormInputs,
-								setUpdateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<input
-						type='text'
-						name='email'
-						placeholder='Email'
-						value={updateAccountFormInputs.email}
-						onChange={(e) =>
-							update(
-								e,
-								updateAccountFormInputs,
-								setUpdateAccountFormInputs,
-							)
-						}
-						onBlur={(e) => validate(e)}
-					/>
-					<button
-						onClick={() =>
-							updateAccount(
-								updateAccountFormInputs.fname,
-								updateAccountFormInputs.lname,
-								updateAccountFormInputs.email,
-							)
-						}
-					>
-						Update Account
-					</button>
-				</article>
-			</div>
-		</div>
-	);
+  const { loginWithEmailAndPassword, createAccount, user, error } = useAuth();
+  const [result, setResult] = useState({});
+  const [loginFormInputs, setLoginFormInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const [createAccountFormInputs, setCreateAccountFormInputs] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+  });
+  const [updateAccountFormInputs, setUpdateAccountFormInputs] = useState({
+    fname: user.fname,
+    lname: user.lname,
+    email: user.email,
+  });
+  const update = (e, inputs, setFunction) => {
+    const {
+      target: { name, value },
+    } = e;
+    setFunction({ ...inputs, [name]: value });
+  };
+  const validate = (e) => {
+    console.log(e.target.value === "hello");
+  };
+  if (error) {
+    console.log("error:", error.message);
+  }
+  const login = async () => {
+    try {
+      return await loginWithEmailAndPassword(
+        loginFormInputs.email,
+        loginFormInputs.password
+      )
+        .then((res) => {
+          console.log("res:", res);
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    } catch {
+      throw new Error("There was an issue logging in.");
+    }
+  };
+  return (
+    <div className={styles.Api}>
+      <h1>API Functions</h1>
+      {error && <p>{JSON.stringify(error)}</p>}
+      <div className={styles.Api__cards}>
+        <article className={styles.Api__cards__card}>
+          <h2>Login</h2>
+          {/* Testing the login function. TODO: Delete this */}
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={loginFormInputs.email}
+            onChange={(e) => update(e, loginFormInputs, setLoginFormInputs)}
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={loginFormInputs.password}
+            onChange={(e) => update(e, loginFormInputs, setLoginFormInputs)}
+            onBlur={(e) => validate(e)}
+          />
+          <button onClick={() => login()}>Login</button>
+          <h4>Result:</h4>
+          {result && JSON.stringify(result)}
+        </article>
+      </div>
+      <div className={styles.Api__cards}>
+        <article className={styles.Api__cards__card}>
+          <h2>Create Account</h2>
+          {/* Testing the login function. TODO: Delete this */}
+          <input
+            type="text"
+            name="fname"
+            placeholder="First Name"
+            value={createAccountFormInputs.fname}
+            onChange={(e) =>
+              update(e, createAccountFormInputs, setCreateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="text"
+            name="lname"
+            placeholder="Last Name"
+            value={createAccountFormInputs.lname}
+            onChange={(e) =>
+              update(e, createAccountFormInputs, setCreateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={createAccountFormInputs.email}
+            onChange={(e) =>
+              update(e, createAccountFormInputs, setCreateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={createAccountFormInputs.password}
+            onChange={(e) =>
+              update(e, createAccountFormInputs, setCreateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <button
+            onClick={() =>
+              createAccount(
+                createAccountFormInputs.fname,
+                createAccountFormInputs.lname,
+                createAccountFormInputs.email,
+                createAccountFormInputs.password
+              )
+            }
+          >
+            Create Account
+          </button>
+        </article>
+      </div>
+      <div className={styles.Api__cards}>
+        <article className={styles.Api__cards__card}>
+          <h2>Update Account</h2>
+          {/* Testing the login function. TODO: Delete this */}
+          <input
+            type="text"
+            name="fname"
+            placeholder="First Name"
+            value={updateAccountFormInputs.fname}
+            onChange={(e) =>
+              update(e, updateAccountFormInputs, setUpdateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="text"
+            name="lname"
+            placeholder="Last Name"
+            value={updateAccountFormInputs.lname}
+            onChange={(e) =>
+              update(e, updateAccountFormInputs, setUpdateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={updateAccountFormInputs.email}
+            onChange={(e) =>
+              update(e, updateAccountFormInputs, setUpdateAccountFormInputs)
+            }
+            onBlur={(e) => validate(e)}
+          />
+          <button
+            onClick={() =>
+              updateAccount(
+                updateAccountFormInputs.fname,
+                updateAccountFormInputs.lname,
+                updateAccountFormInputs.email
+              )
+            }
+          >
+            Update Account
+          </button>
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default Apis;
--- frontend/providers/auth.js
+++ frontend/providers/auth.js
@@ -1,106 +1,132 @@
-import { createContext, useContext, useState } from 'react';
-import BULB_API from '../config/api';
+import { createContext, useContext, useState } from "react";
+import BULB_API from "../config/api";
 
 const AuthContext = createContext({
-	isAuthenticated: false,
-	user: {},
-	login: () => {},
-	logout: () => {},
-	createAccount: () => {},
-	updateAccount: () => {},
-	deleteAccount: () => {},
-	updateUser: () => {},
-	updatePassword: () => {},
+  isAuthenticated: false,
+  user: {},
+  login: () => {},
+  logout: () => {},
+  createAccount: () => {},
+  updateAccount: () => {},
+  deleteAccount: () => {},
+  updateUser: () => {},
+  updatePassword: () => {},
 });
 
 export const AuthProvider = ({ children }) => {
-	const [user, setUser] = useState({
-		isAuthenticated: false,
-		data: {},
-	});
-	const [error, setError] = useState(null);
-	const login = (email, password) => {
-		console.log(email, password);
-		if (email && password) {
-			BULB_API.post('/auth/login', { email, password })
-				.then((res) => {
-					console.log('login result', res);
-					if (res.data) {
-						setUser({
-							isAuthenticated: true,
-							data: res.data.user,
-						});
-					}
-					if (res.error) {
-						console.log('API Error:', res.error);
-					}
-				})
-				.catch((err) => {
-					console.log('error:', err);
-					setError(err);
-				});
-		}
-	};
-	const createAccount = (fname, lname, email, password) => {
-		if (fname && lname && email && password) {
-			BULB_API.post('/createAccount', {
-				fname,
-				lname,
-				email,
-				password,
-			})
-				.then((res) => {
-					if (res.status === 201) {
-						setUser(res.data.user);
-						console.log(res.data);
-					} else {
-						setError(
-							'something went wrong creating this user',
-							res.data,
-						);
-					}
-				})
-				.catch((err) => {
-					setError(err);
-				});
-		} else {
-			setError('missing info');
-		}
-	};
-	const updateAccount = () => {
-		console.log('Update Account');
-	};
-	const logout = () => {
-		console.log('logging out');
-		BULB_API.get('/auth/logout')
-			.then((res) => {
-				console.log('logout result:', res.data);
-				if (res.data) {
-					setUser({
-						isAuthenticated: false,
-						data: {},
-					});
-				}
-			})
-			.catch((err) => {
-				setError(err);
-			});
-	};
-	return (
-		<AuthContext.Provider
-			value={{
-				isUserAuthenticated: user.isAuthenticated,
-				user: user.data,
-				login,
-				logout,
-				createAccount,
-				updateAccount,
-				error,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  const [user, setUser] = useState({
+    isAuthenticated: false,
+    data: {},
+  });
+
+  const loginWithEmailAndPassword = (email, password) => {
+    return new Promise((resolve, reject) => {
+      if (!email || !password) {
+        return reject({
+          error: { message: "Email and password are required to login" },
+        });
+      }
+
+      try {
+        const response = BULB_API.post("/auth/login", { email, password });
+        console.log("bulbapi response", response);
+        if (!response) {
+          return reject({
+            error: { message: "These credentials are invalid" },
+          });
+        }
+        if (response.error) {
+          return reject(response.error);
+        }
+
+        resolve(response.data);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const [error, setError] = useState(null);
+  const login = (email, password) => {
+    console.log(email, password);
+    if (email && password) {
+      BULB_API.post("/auth/login", { email, password })
+        .then((res) => {
+          console.log("login result", res);
+          if (res.data) {
+            setUser({
+              isAuthenticated: true,
+              data: res.data.user,
+            });
+          }
+          if (res.error) {
+            console.log("API Error:", res.error);
+          }
+        })
+        .catch((err) => {
+          console.log("error:", err);
+          setError(err);
+        });
+    }
+  };
+  const createAccount = (fname, lname, email, password) => {
+    if (fname && lname && email && password) {
+      BULB_API.post("/createAccount", {
+        fname,
+        lname,
+        email,
+        password,
+      })
+        .then((res) => {
+          if (res.status === 201) {
+            setUser(res.data.user);
+            console.log(res.data);
+          } else {
+            setError("something went wrong creating this user", res.data);
+          }
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    } else {
+      setError("missing info");
+    }
+  };
+  const updateAccount = () => {
+    console.log("Update Account");
+  };
+  const logout = () => {
+    console.log("logging out");
+    BULB_API.get("/auth/logout")
+      .then((res) => {
+        console.log("logout result:", res.data);
+        if (res.data) {
+          setUser({
+            isAuthenticated: false,
+            data: {},
+          });
+        }
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isUserAuthenticated: user.isAuthenticated,
+        user: user.data,
+        login,
+        loginWithEmailAndPassword,
+        logout,
+        createAccount,
+        updateAccount,
+        error,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
