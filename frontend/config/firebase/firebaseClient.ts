cb5dca9b6157461e23b87e703edd7c6e099bd4ca
Christopher Jones <tkcwebdev@gmail.com>
2022-07-22 09:46:54 -0700

    19: Firebase Client: Setup correct way to use emulator while running

--- frontend/config/firebase/firebaseClient.ts
+++ frontend/config/firebase/firebaseClient.ts
@@ -1,15 +1,20 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
-  firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+firebase.initializeApp({
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
 	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
 	appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-  });
-
+});
+/**
+ * Firebase authentication instance with the required credentials.
+ * Authentication emulator is set by the firebase env variable.
+ */
 const auth = firebase.auth();
+
+
 
 export default auth;
