d780735fe9107c06a6bd314ca2652b676d2c718b
Christopher Jones <tkcwebdev@gmail.com>
2022-07-25 23:08:53 -0700

    19: Dashboard: Setup file structure to match the rest of the project.

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,16 +1,16 @@
-import styles from '../styles/Dashboard.module.sass';
-import fullColorLogo from '../public/logo-full-color.png';
-import Head from 'next/head';
-import Image from 'next/image';
-import IdeaList from '../components/IdeaList/IdeaList';
+import Head from "next/head";
+import Image from "next/image";
+import IdeaList from "../../components/IdeaList/IdeaList";
+import fullColorLogo from "../../public/bulb_full_color.svg";
+import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Explore bulb</title>
-        <meta name='description' content='Dashboard page' />
-        <link rel='icon' href='/bulb-favicon.ico' />
+        <meta name="description" content="Dashboard page" />
+        <link rel="icon" href="/bulb-favicon.ico" />
       </Head>
 
       <main className={styles.main}>
@@ -20,7 +20,7 @@ export default function Dashboard() {
 
       <footer className={styles.footer}>
         <span className={styles.logo}>
-          <Image src={fullColorLogo} alt='bulb Logo' width={144} height={64} />
+          <Image src={fullColorLogo} alt="bulb Logo" width={144} height={64} />
         </span>
       </footer>
     </div>
