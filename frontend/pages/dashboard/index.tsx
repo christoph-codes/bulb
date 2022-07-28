9d3a9fa27c7b1629e1891adcb9813d4576082442
Christopher Jones <tkcwebdev@gmail.com>
2022-07-28 00:55:07 -0700

    21: Dashboard: USe Dashboard Layout template for the page

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -2,27 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import IdeaList from "../../components/IdeaList/IdeaList";
 import fullColorLogo from "../../public/bulb_full_color.svg";
+import DashboardLayout from "../../templates/DashboardLayout";
 import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
   return (
-    <div className={styles.container}>
+    <DashboardLayout title="Current Ideas" className={styles.Dashboard}>
       <Head>
         <title>Explore bulb</title>
         <meta name="description" content="Dashboard page" />
         <link rel="icon" href="/bulb-favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>Here are your current Ideas:</h1>
         <IdeaList />
       </main>
-
-      <footer className={styles.footer}>
-        <span className={styles.logo}>
-          <Image src={fullColorLogo} alt="bulb Logo" width={144} height={64} />
-        </span>
-      </footer>
-    </div>
+    </DashboardLayout>
   );
 }
