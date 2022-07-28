6382020778eda7648cd0e31536b79719a89d3bfe
Christopher Jones <tkcwebdev@gmail.com>
2022-07-28 01:11:56 -0700

    21: Dashboard Page: Change title from current ideas to my ideas

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import IdeaList from "../../components/IdeaList/IdeaList";
-import fullColorLogo from "../../public/bulb_full_color.svg";
 import DashboardLayout from "../../templates/DashboardLayout";
 import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
   return (
-    <DashboardLayout title="Current Ideas" className={styles.Dashboard}>
+    <DashboardLayout title="My Ideas" className={styles.Dashboard}>
       <Head>
         <title>Explore bulb</title>
         <meta name="description" content="Dashboard page" />
