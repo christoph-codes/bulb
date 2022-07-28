05de8e1410bafd1c9b4a94f53a1adf92a2743ee3
Christopher Jones <tkcwebdev@gmail.com>
2022-07-27 22:34:23 -0700

    19: Helper functions: Update to include session storage instead of local

--- frontend/utils/helper.ts
+++ frontend/utils/helper.ts
@@ -14,13 +14,14 @@ export const convertTimestamp = (timestamp: any) => {
 };
 
 /**
- * Sets a local storage variable with a value and an expiration date.
- * @param key string of the key of the local storage value.
+ * Sets a session storage variable with a value and an expiration date.
+ * @param key string of the key of the session storage value.
  * @param value can be any value that is wanted to save.
- * @param ttl The amount of time in milliseconds for the local storage values to be stored
- * @return Sets the value to a storage element in local storage.
+ * @param ttl The amount of time in milliseconds for the session storage values to be stored
+ * @return Sets the value to a storage element in session storage.
  */
 export const setWithExpiry = (key: string, value: any, ttl: number) => {
+	const ISSERVER = typeof window === "undefined";
 	const now = new Date();
 
 	// `item` is an object which contains the original value
@@ -30,39 +31,47 @@ export const setWithExpiry = (key: string, value: any, ttl: number) => {
 		expiry: now.getTime() + ttl,
 	};
 	if (!ttl) {
-		localStorage.setItem(key, JSON.stringify(value));
+		if (!ISSERVER) {
+			window.sessionStorage.setItem(key, JSON.stringify(value));
+		}
 	} else {
-		localStorage.setItem(key, JSON.stringify(item));
+		window.sessionStorage.setItem(key, JSON.stringify(item));
 	}
 };
 /**
- * Getter for a given local storage variable
- * @param key String of the variable saved in local storage
- * @returns returns the value of what was saved inside of a given local storage variable
+ * Getter for a given session storage variable
+ * @param key String of the variable saved in session storage
+ * @returns returns the value of what was saved inside of a given session storage variable
  */
 export const getWithExpiry = (key: string) => {
-	const itemStr = localStorage.getItem(key);
-	// if the item doesn't exist, return null
-	if (!itemStr) {
-		return null;
-	}
-	const item = JSON.parse(itemStr);
-	const now = new Date();
-	if (item.expiry) {
-		// compare the expiry time of the item with the current time
-		if (now.getTime() > item.expiry) {
-			// If the item is expired, delete the item from storage
-			// and return null
-			localStorage.removeItem(key);
+	const ISSERVER = typeof window === "undefined";
+	if (!ISSERVER) {
+		const itemStr = window.sessionStorage.getItem(key);
+		// if the item doesn't exist, return null
+		if (!itemStr) {
 			return null;
 		}
+		const item = JSON.parse(itemStr);
+		const now = new Date();
+		if (item.expiry) {
+			// compare the expiry time of the item with the current time
+			if (now.getTime() > item.expiry) {
+				// If the item is expired, delete the item from storage
+				// and return null
+				window.sessionStorage.removeItem(key);
+				return null;
+			}
+		}
+		return item.value;
 	}
-	return item.value;
 };
 /**
- * Kills the key and value of a local storage element.
- * @param key The string that the data was stored under in local storage
+ * Kills the key and value of a session storage element.
+ * @param key The string that the data was stored under in session storage
  */
 export const clearItem = (key: string) => {
-	localStorage.removeItem(key);
+	const ISSERVER = typeof window === "undefined";
+	if (!ISSERVER) {
+		window.sessionStorage.removeItem(key);
+	}
 };
