6cd6aee8fc8abd247db913c317041e983b08a313
Christopher Jones <tkcwebdev@gmail.com>
2022-07-27 22:30:49 -0700

    19: User API: Create new user and save to database route for api

--- frontend/pages/api/users/create.ts
+++ frontend/pages/api/users/create.ts
@@ -1,9 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import auth from "../../../config/firebase/firebaseClient";
 import db from "../../../config/mongodb";
 
 export default async (req: NextApiRequest, res: NextApiResponse, next: any) => {
-  // TODO: Create route for adding a user to mongodb
-	const { user } = req.body;
-  console.log("user", user);
+	const {user} = req.body;
+  if (user) {
+	  try {
+		  const dbUsers = await db('users');
+		  const results = await dbUsers.insertOne(user);
+		  if (results.insertedId) {
+			  res.status(201).send({
+				  message: 'User created successfully',
+				  user,
+			  });
+		  } else {
+			  res.status(400).send({
+				  error: { message: 'No user was written to the database.' },
+			  });
+		  }
+	  } catch (err: any) {
+		  if (err) {
+			  res.status(500).send({
+				  error: { message: err.message },
+			  });
+		  }
+		  res.status(500).send({
+			  error: {
+				  message:
+					  'There was an issue creating the user on the server.',
+			  },
+		  });
+	  }
+  } else {
+	  res.status(400).send({
+		  error: {
+			  message: 'A valid email is required to create a new user.',
+		  },
+	  });
+  }
 };
