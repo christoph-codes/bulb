f016db22e4bc9ac49333f5ee32dda66090a82fd3
Christopher Jones <tkcwebdev@gmail.com>
2022-07-29 00:34:24 -0700

    21: Dashboard: Update page to only use content of page. No need for SEO

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,19 +1,11 @@
-import Head from "next/head";
 import IdeaList from "../../components/IdeaList/IdeaList";
 import DashboardLayout from "../../templates/DashboardLayout";
 import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
-  return (
-    <DashboardLayout title="My Ideas" className={styles.Dashboard}>
-      <Head>
-        <title>Explore bulb</title>
-        <meta name="description" content="Dashboard page" />
-        <link rel="icon" href="/bulb-favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <IdeaList />
-      </main>
-    </DashboardLayout>
-  );
+	return (
+		<DashboardLayout title="My Ideas" className={styles.Dashboard}>
+			<IdeaList />
+		</DashboardLayout>
+	);
 }
