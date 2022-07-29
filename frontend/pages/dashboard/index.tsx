eb083009ef34b2a471eefe8e669a9590c4f85b3c
Christopher Jones <tkcwebdev@gmail.com>
2022-07-29 01:06:17 -0700

    21: Dashboard: Add single utility button to the main idea page to add a new one. WIP

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,11 +1,33 @@
 import Head from "next/head";
+import { MdAddCircleOutline } from "react-icons/md";
 import IdeaList from "../../components/IdeaList/IdeaList";
-import DashboardLayout from "../../templates/DashboardLayout";
+import DashboardLayout, {
+	TUtilityButton,
+} from "../../templates/DashboardLayout";
 import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
+	const addIdea = () => {
+		console.log("adding idea...");
+	};
+	const utilityButtons: TUtilityButton[] = [
+		{
+			label: (
+				<>
+					<MdAddCircleOutline /> add idea
+				</>
+			),
+			onClick: () => addIdea(),
+			variant: "primary",
+			chip: true,
+		},
+	];
 	return (
-		<DashboardLayout title="My Ideas" className={styles.Dashboard}>
+		<DashboardLayout
+			title="My Ideas"
+			className={styles.Dashboard}
+			utilityButtons={utilityButtons}
+		>
 			<Head>
 				<title>My Ideas » Bulb</title>
 				<meta name="description" content="My Ideas page" />
