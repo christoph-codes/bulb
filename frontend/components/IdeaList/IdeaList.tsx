de398f58e10a620d654415de2dab09f50eb2a4e3
Christopher Jones <tkcwebdev@gmail.com>
2022-08-01 00:13:48 -0700

    26: IdeaList: Remove fetch logic from idea list and add as a prop. Moved logic to the new provider

--- frontend/components/IdeaList/IdeaList.tsx
+++ frontend/components/IdeaList/IdeaList.tsx
@@ -1,50 +1,29 @@
 import IdeaNote from "../IdeaNote/IdeaNote";
-import { useEffect, useState } from "react";
-import router from "next/router";
 import styles from "./IdeaList.module.scss";
-import axios from "axios";
-import { useAuth } from "../../providers/AuthProvider";
 
-type Idea = {
-  name: string;
-  description: string;
+type TIdea = {
+	name: string;
+	description: string;
 };
+export interface IIdeaListProps {
+	ideas: TIdea[];
+}
 
-const IdeaList = () => {
-  const { user } = useAuth();
-  const [ideaList, setIdeaList] = useState([]);
-
-  const getIdeasFromApi = () => {
-    axios
-      .post("/api/ideas/user", { user: { id: user?._id } })
-      .then(async (response) => {
-        if (response.status === 200) {
-          const data = await response.data.result;
-          setIdeaList(data);
-        } else if (response.status === 401) {
-          router.push("/");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  useEffect(() => {
-    getIdeasFromApi();
-  }, []);
-
-  return (
-    <ul className={styles.IdeaList}>
-      {ideaList.map((idea: Idea, i: number) => {
-        return (
-          <li key={i}>
-            <IdeaNote name={idea.name} description={idea.description} />
-          </li>
-        );
-      })}
-    </ul>
-  );
+const IdeaList = ({ ideas }: IIdeaListProps) => {
+	return (
+		<ul className={styles.IdeaList}>
+			{ideas?.map((idea: TIdea, i: number) => {
+				return (
+					<li key={i}>
+						<IdeaNote
+							name={idea.name}
+							description={idea.description}
+						/>
+					</li>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default IdeaList;
