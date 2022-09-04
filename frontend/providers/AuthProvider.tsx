7c780ee21a8c05fb625098633ce1421d8ee49167
Christopher Jones <tkcwebdev@gmail.com>
2022-09-04 09:59:19 -0700

    43: Auth: Add logout function to the auth provider

--- frontend/providers/AuthProvider.tsx
+++ frontend/providers/AuthProvider.tsx
@@ -1,78 +1,98 @@
-import { useRouter } from "next/router";
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/router';
 import {
-  createContext,
-  useState,
-  useEffect,
-  useContext,
-  Dispatch,
-  SetStateAction,
-} from "react";
-import { getWithExpiry, setWithExpiry } from "../utils/helper";
+	createContext,
+	useState,
+	useEffect,
+	useContext,
+	Dispatch,
+	SetStateAction,
+} from 'react';
+import auth from '../config/firebase/firebaseClient';
+import { getWithExpiry, setWithExpiry } from '../utils/helper';
 
 export interface IAuthContext {
-  user: IUser | null;
-  setUser: Dispatch<SetStateAction<IUser | null>>;
+	user: IUser | null;
+	setUser: Dispatch<SetStateAction<IUser | null>>;
+	logout: () => void;
 }
 export interface IUser {
-  _id?: string;
-  username?: string;
-  fname?: string;
-  lname?: string;
-  email?: string;
-  githubUrl?: string;
-  jobTitle?: string;
-  bio?: string;
-  lastLoggedInDate?: Date | null;
+	_id?: string;
+	username?: string;
+	fname?: string;
+	lname?: string;
+	email?: string;
+	githubUrl?: string;
+	jobTitle?: string;
+	bio?: string;
+	lastLoggedInDate?: Date | null;
 }
 
 const AuthContext = createContext<IAuthContext>({
-  user: {
-    _id: "",
-    username: "",
-    fname: "",
-    lname: "",
-    email: "",
-    githubUrl: "",
-    jobTitle: "",
-    bio: "",
-    lastLoggedInDate: null,
-  },
-  setUser: () => {},
+	user: {
+		_id: '',
+		username: '',
+		fname: '',
+		lname: '',
+		email: '',
+		githubUrl: '',
+		jobTitle: '',
+		bio: '',
+		lastLoggedInDate: null,
+	},
+	setUser: () => {},
+	logout: () => {},
 });
 
 const AuthProvider = ({ children }: any) => {
-  const router = useRouter();
-  const [user, setUser] = useState<IUser | null>(() => {
-    const localUser: IUser = getWithExpiry("bulb_user");
-    return (
-      localUser || {
-        _id: "",
-        username: "",
-        fname: "",
-        lname: "",
-        email: "",
-        githubUrl: "",
-        jobTitle: "",
-        bio: "",
-        lastLoggedInDate: "",
-      }
-    );
-  });
-  useEffect(() => {
-    setWithExpiry("bulb_user", user, 3600000);
-  }, [user]);
+	const router = useRouter();
+	const [user, setUser] = useState<IUser | null>(() => {
+		const localUser: IUser = getWithExpiry('bulb_user');
+		return (
+			localUser || {
+				_id: '',
+				username: '',
+				fname: '',
+				lname: '',
+				email: '',
+				githubUrl: '',
+				jobTitle: '',
+				bio: '',
+				lastLoggedInDate: '',
+			}
+		);
+	});
+	useEffect(() => {
+		setWithExpiry('bulb_user', user, 3600000);
+	}, [user]);
 
-  useEffect(() => {
-    if (!user?._id) {
-      router.push("/login");
-    }
-  }, []);
+	useEffect(() => {
+		if (!user?._id) {
+			router.push('/login');
+		}
+	}, []);
 
-  return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	const logout = () => {
+		signOut(auth).then((data) => {
+			console.log('signout data', data);
+			setUser({
+				_id: '',
+				username: '',
+				fname: '',
+				lname: '',
+				email: '',
+				githubUrl: '',
+				jobTitle: '',
+				bio: '',
+			});
+		});
+	};
+
+	return (
+		<AuthContext.Provider value={{ user, setUser, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export const useAuth = () => useContext(AuthContext);
