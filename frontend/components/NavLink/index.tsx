e6f6a1174f4d8e6b0e0bee49840643bcc25e5c91
Christopher Jones <tkcwebdev@gmail.com>
2022-09-07 21:36:40 -0700

    43: NavLink component: Use href from url type from link props

--- frontend/components/NavLink/index.tsx
+++ frontend/components/NavLink/index.tsx
@@ -1,65 +1,68 @@
-import { useRouter } from "next/router";
-import Link, { LinkProps } from "next/link";
-import React, { useState, useEffect, ReactElement, Children } from "react";
+import { useRouter } from 'next/router';
+import Link, { LinkProps } from 'next/link';
+import React, { useState, useEffect, ReactElement, Children } from 'react';
+import { Url } from 'url';
 
 export interface INavLinkProps extends LinkProps {
-  children: ReactElement;
-  activeClassName: string;
-  newTab?: boolean;
+	children: ReactElement;
+	activeClassName: string;
+	newTab?: boolean;
+	href: Url;
 }
 
 const NavLink = ({
-  children,
-  activeClassName,
-  newTab,
-  ...props
+	children,
+	activeClassName,
+	newTab,
+	href,
+	...props
 }: INavLinkProps) => {
-  const { asPath, isReady } = useRouter();
+	const { asPath, isReady } = useRouter();
 
-  const child = Children.only(children);
-  const childClassName = child.props.className || "";
-  const [className, setClassName] = useState(childClassName);
+	const child = Children.only(children);
+	const childClassName = child.props.className || '';
+	const [className, setClassName] = useState(childClassName);
 
-  useEffect(() => {
-    // Check if the router fields are updated client-side
-    if (isReady) {
-      // Dynamic route will be matched via props.as
-      // Static route will be matched via props.href
-      const linkPathname = new URL(
-        (props.as || props.href) as string,
-        location.href
-      ).pathname;
+	useEffect(() => {
+		// Check if the router fields are updated client-side
+		if (isReady) {
+			// Dynamic route will be matched via props.as
+			// Static route will be matched via props.href
+			const linkPathname = new URL(
+				(props.as || href) as string,
+				location.href,
+			).pathname;
 
-      // Using URL().pathname to get rid of query and hash
-      const activePathname = new URL(asPath, location.href).pathname;
+			// Using URL().pathname to get rid of query and hash
+			const activePathname = new URL(asPath, location.href).pathname;
 
-      const newClassName =
-        linkPathname === activePathname
-          ? `${childClassName} ${activeClassName}`.trim()
-          : childClassName;
+			const newClassName =
+				linkPathname === activePathname
+					? `${childClassName} ${activeClassName}`.trim()
+					: childClassName;
 
-      if (newClassName !== className) {
-        setClassName(newClassName);
-      }
-    }
-  }, [
-    asPath,
-    isReady,
-    props.as,
-    props.href,
-    childClassName,
-    activeClassName,
-    setClassName,
-    className,
-  ]);
+			if (newClassName !== className) {
+				setClassName(newClassName);
+			}
+		}
+	}, [
+		asPath,
+		isReady,
+		props.as,
+		href,
+		childClassName,
+		activeClassName,
+		setClassName,
+		className,
+	]);
 
-  return (
-    <Link target={newTab ? "_blank" : "_self"} {...props}>
-      {React.cloneElement(child, {
-        className: className || null,
-      })}
-    </Link>
-  );
+	return (
+		<Link href={href} target={newTab ? '_blank' : '_self'} {...props}>
+			{React.cloneElement(child, {
+				className: className || null,
+			})}
+		</Link>
+	);
 };
 
 export default NavLink;
